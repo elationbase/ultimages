aef2af613933a987e8722b4c83874bbdea99eff5
elationbase <elationbase@gmail.com>
2016-06-13 13:02:01 -0400

    sticky elements UI

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -9916,7 +9916,36 @@ c.event,c.classes,c.options)}return function(a){if(a.from&&a.to){var b=d(a.from)
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
-	
+
+		/*
+			Stick elements
+		*/
+		var winTop = $(window).scrollTop();
+		var stickClass = 'js-stick-on';
+		var offset, pos;
+
+		var addStick = function(offset) {
+
+			winTop = $(window).scrollTop();
+
+			$('.js-stick-ele').each(function() {
+
+				offset = $(this).data('offset');
+				pos = $(this).position().top - winTop;
+
+				if (pos < offset) {
+					if (!$(this).hasClass(stickClass)) {
+						$(this).addClass(stickClass);
+					}
+				} else {
+					if ($(this).hasClass(stickClass)) {
+						$(this).removeClass(stickClass);
+					}
+				}
+			});
+		};
+
+		$(window).on('scroll', addStick);
 
 	});
 
--- components/scripts/global.js
+++ components/scripts/global.js
@@ -3,7 +3,36 @@
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
-	
+
+		/*
+			Stick elements
+		*/
+		var winTop = $(window).scrollTop();
+		var stickClass = 'js-stick-on';
+		var offset, pos;
+
+		var addStick = function(offset) {
+
+			winTop = $(window).scrollTop();
+
+			$('.js-stick-ele').each(function() {
+
+				offset = $(this).data('offset');
+				pos = $(this).position().top - winTop;
+
+				if (pos < offset) {
+					if (!$(this).hasClass(stickClass)) {
+						$(this).addClass(stickClass);
+					}
+				} else {
+					if ($(this).hasClass(stickClass)) {
+						$(this).removeClass(stickClass);
+					}
+				}
+			});
+		};
+
+		$(window).on('scroll', addStick);
 
 	});
 
